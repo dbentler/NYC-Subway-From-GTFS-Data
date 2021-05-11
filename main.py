d3b6459272fbe60803237f017259f920fff83fb1
dbentler <db3857@nyu.edu>
2021-05-11 16:03:21 -0400

    Corrected file read issue

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 """
 Darren Bentler 2021 - Using the MTA's GTFS data available online, I plot and draw the NYC subway.
 """
+import matplotlib.pyplot as plt
 
 class Station:
-    stat_id = ""
-    stat_lat = 0.0
-    stat_long = 0.0
     def __init__(self, stat_id, stat_lat, stat_long):
         self.stat_id = stat_id
         self.stat_lat = stat_lat
@@ -22,17 +20,21 @@ def parse_data(file):
     """
     with open(file, "r") as f:
         stations = []
-        for line in file:
+        while True:
             line = f.readline()
-            if "shape_id" in line: # Passes the first line in shapes.txt, which is not needed.
+            if not line:
+                break
+            elif "shape_id" in line:
                 pass
             else:
-                f_mat = line.split(",")
-                stations.append(Station(f_mat[0], f_mat[1], f_mat[2])) # We already know from shapes.txt that the format is "Shape ID", "Shape Point Latitude", "Shape point Longitude"
+                f_mat = list(line.split(","))
+                tmp_stat = Station(f_mat[0], f_mat[1], f_mat[2])
+                stations.append(tmp_stat)
         return stations
 
 
 if __name__ == "__main__":
     stations = parse_data("K:\\!dev\\Python\\nyc_subway2\\NYC-Subway-From-GTFS-Data\\shapes.txt") # You'll need to copy your own path to the shapes.txt file here.
-    print(stations)
+    for obj in stations:
+        print(obj.stat_lat, obj.stat_long)
 
