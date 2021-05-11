0401aa3ae6bd1b3ba029221928eb155f15dca5bc
dbentler <db3857@nyu.edu>
2021-05-11 15:17:52 -0400

    Successfully parsed data

--- main.py
+++ main.py
@@ -14,7 +14,25 @@ class Station:
     def __repr__(self):
         return f"{self.stat_id}, {self.stat_lat}, {self.stat_long}"
 
+def parse_data(file):
+    """
+    Parses through stops.txt and grabs all of the necessary data about a route's stops.
+    Returns a list of Stations.
+    string --> list(Station)
+    """
+    with open(file, "r") as f:
+        stations = []
+        for line in file:
+            line = f.readline()
+            if "shape_id" in line: # Passes the first line in shapes.txt, which is not needed.
+                pass
+            else:
+                f_mat = line.split(",")
+                stations.append(Station(f_mat[0], f_mat[1], f_mat[2])) # We already know from shapes.txt that the format is "Shape ID", "Shape Point Latitude", "Shape point Longitude"
+        return stations
+
+
 if __name__ == "__main__":
-    print("Hello World")
-    test_station = Station("A", -40.0, -71.00)
-    print(test_station)
+    stations = parse_data("K:\\!dev\\Python\\nyc_subway2\\NYC-Subway-From-GTFS-Data\\shapes.txt") # You'll need to copy your own path to the shapes.txt file here.
+    print(stations)
+
